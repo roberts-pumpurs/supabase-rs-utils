cff83ee9a027e2450ab3db5edbece37dd7419b24
okristaps <107648450+okristaps@users.noreply.github.com>
2024-08-28 20:04:40 +0300

    postgres_changes

--- crates/supabase-realtime/src/message.rs
+++ crates/supabase-realtime/src/message.rs
@@ -16,6 +16,9 @@ pub enum ProtocolMesseage {
     System(PhoenixMessage<system::System>),
     #[serde(rename = "phx_error")]
     PhxError(PhoenixMessage<phx_error::PhxError>),
+    #[serde(rename = "postgres_changes")]
+    PostgresChanges(PhoenixMessage<postgres_changes::PostgresChangesPayload>),
+
 }
 
 impl ProtocolMesseage {
@@ -33,6 +36,7 @@ impl ProtocolMesseage {
             ProtocolMesseage::PresenceState(_) => {}
             ProtocolMesseage::System(_) => {}
             ProtocolMesseage::PhxError(_) => {}
+            ProtocolMesseage::PostgresChanges(_) => {}
         }
     }
 }
@@ -563,41 +567,129 @@ pub mod phx_error {
     }
 }
 
-//     {
-//   "event": "phx_error",
-//   "payload": {},
-//   "ref": "1",
-//   "topic": "realtime:db"
-// }
+
+pub mod postgres_changes {
+    use super::*;
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct PostgresChangesPayload {
+        pub data: Data,
+        pub ids: Vec<i64>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct Data {
+        pub columns: Vec<Column>,
+        #[serde(rename = "commit_timestamp")]
+        pub commit_timestamp: String,
+        pub errors: Option<String>,
+        #[serde(rename = "old_record")]
+        pub old_record: Option<Record>,
+        pub record: Record,
+        pub schema: String,
+        pub table: String,
+        #[serde(rename = "type")]
+        pub type_: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct Column {
+        pub name: String,
+        #[serde(rename = "type")]
+        pub type_: String, 
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct Record {
+        #[serde(rename = "id")]
+        pub id: String,
+        #[serde(rename = "updated_at")]
+        pub updated_at: Option<String>, 
+        #[serde(rename = "url")]
+        pub url: Option<String>, 
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_postgres_changes_serialization() {
+            let json_data = r#"
+            {
+                "event": "postgres_changes",
+                "payload": {
+                    "data": {
+                        "columns": [
+                            {"name": "id", "type": "uuid"},
+                            {"name": "updated_at", "type": "timestamptz"},
+                            {"name": "url", "type": "text"}
+                        ],
+                        "commit_timestamp": "2024-08-25T17:00:19.009Z",
+                        "errors": null,
+                        "old_record": {
+                            "id": "96236356-5ac3-4403-b3ce-c660973330d9"
+                        },
+                        "record": {
+                            "id": "96236356-5ac3-4403-b3ce-c660973330d9",
+                            "updated_at": "2024-08-25T17:00:19.005328+00:00",
+                            "url": "https://0.0.0.0:3334"
+                        },
+                        "schema": "public",
+                        "table": "profiles",
+                        "type": "UPDATE"
+                    },
+                    "ids": [38606455]
+                },
+                "ref": null,
+                "topic": "realtime:db"
+            }
+            "#;
+
+            let expected_struct = ProtocolMesseage::PostgresChanges(PhoenixMessage {
+                topic: "realtime:db".to_string(),
+                payload: PostgresChangesPayload {
+                    data: Data {
+                        columns: vec![
+                            Column { name: "id".to_string(), type_: "uuid".to_string() },
+                            Column { name: "updated_at".to_string(), type_: "timestamptz".to_string() },
+                            Column { name: "url".to_string(), type_: "text".to_string() },
+                        ],
+                        commit_timestamp: "2024-08-25T17:00:19.009Z".to_string(),
+                        errors: None,
+                        old_record: Some(Record {
+                            id: "96236356-5ac3-4403-b3ce-c660973330d9".to_string(),
+                            updated_at: None,
+                            url: None,
+                        }),
+                        record: Record {
+                            id: "96236356-5ac3-4403-b3ce-c660973330d9".to_string(),
+                            updated_at: Some("2024-08-25T17:00:19.005328+00:00".to_string()),
+                            url: Some("https://0.0.0.0:3334".to_string()),
+                        },
+                        schema: "public".to_string(),
+                        table: "profiles".to_string(),
+                        type_: "UPDATE".to_string(),
+                    },
+                    ids: vec![38606455],
+                },
+                ref_field: None,
+                join_ref: None,
+            });
+
+            let serialized = serde_json::to_value(&expected_struct).unwrap();
+            dbg!(serialized);
+
+            let deserialized_struct: ProtocolMesseage = serde_json::from_str(json_data).unwrap();
+            dbg!(&deserialized_struct);
+
+            assert_eq!(deserialized_struct, expected_struct);
+        }
+    }
+}
 
 
-
-// todo: handle this
-//     {
-//   "event": "postgres_changes",
-//   "payload": {
-//     "data": {
-//       "columns": [
-//         {"name": "id", "type": "uuid"},
-//         {"name": "updated_at", "type": "timestamptz"},
-//         {"name": "url", "type": "text"},
-//       ],
-//       "commit_timestamp": "2024-08-25T17:00:19.009Z",
-//       "errors": null,
-//       "old_record": {
-//         "id": "96236356-5ac3-4403-b3ce-c660973330d9"
-//       },
-//       "record": {
-//         "id": "96236356-5ac3-4403-b3ce-c660973330d9",
-//         "updated_at": "2024-08-25T17:00:19.005328+00:00",
-//         "url": "https://0.0.0.0:3334",
-//       },
-//       "schema": "public",
-//       "table": "profiles",
-//       "type": "UPDATE"
-//     },
-//     "ids": [38606455]
-//   },
-//   "ref": null,
-//   "topic": "realtime:db"
-// }
